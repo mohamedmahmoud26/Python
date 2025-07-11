37117e177c62da05e94b051e689ce8eee21e5777
Mohamed Elseragy <mohamedmahmoud2682003@gmail.com>
2025-07-11 23:19:41 +0300

    Add files via upload

---
+++ Tuples/tuples.py
@@ -0,0 +1,8 @@
+t=(1,2,6)
+print(type(t)) #<class 'tuple'>
+
+# index and count
+t=('mohamed' , 'ali', 'ahmed',4,4,5,58,12,1,1,1)
+print(t.index(1)) #8
+print(t.index('mohamed')) #0
+print(t.count(1)) #3
